8ef097bac524eaf0d165c8cfe49ff081f29931f2
wangchaochuan <1277949393@qq.com>
2020-04-04 17:22:58 +0800

    优化webpack打包

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
+const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
 module.exports = {
+    productionSourceMap: false, // 生产打包时不输出map文件，增加打包速度
     pwa: {
         iconPaths: {
             favicon32: "favicon.ico",
@@ -8,7 +11,29 @@ module.exports = {
             msTileImage: "favicon.ico"
         }
     },
-    css: {
-        extract: true
+    configureWebpack: {
+        module: {
+            rules: [{
+                test: /\.js$/,
+                use: ["thread-loader", 'babel-loader'],
+                include: path.resolve('src'),
+                exclude: /node_modules/
+            }]
+        },
+        // 缩小你的JavaScript 生产环境删除console.log。
+        optimization: {
+            minimizer: [
+                new TerserPlugin({
+                    // pure_funcs: ['console.log'],
+                    cache: true, //开启文件缓存
+                    parallel: true, //开启并发 也也可以指定并发数
+                }),
+            ],
+        }
+    },
+    chainWebpack: config => {
+        config
+            .plugin('webpack-bundle-analyzer')
+            .use(require('webpack-bundle-analyzer').BundleAnalyzerPlugin)
     }
 };
